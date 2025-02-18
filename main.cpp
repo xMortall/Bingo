97a2a9c5ee3ee0c3cef9c5234ed6c837d6fe6773
jose-gomes1 <145759337+jose-gomes1@users.noreply.github.com>
2025-02-18 14:10:09 -0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/**
+ * @authors Emanuel Borges, José Gomes
+ * @brief Main file para o jogo do Bingo
+ * @date 18-02-2025
+ * @file main.cpp
+ */
+
+#include "bingo.h"
+
+int main(){
+    Bingo Bingo{};
+    return 0;
+}
+
+//notas: usar param para variáveis e brief para funções

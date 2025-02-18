0913751c3228bdcc6e598150539a69f00085d2d4
jose-gomes1 <145759337+jose-gomes1@users.noreply.github.com>
2025-02-18 14:09:46 -0100

    Create bingo.h

---
+++ bingo.h
@@ -0,0 +1,18 @@
+/**
+ * @authors Emanuel Borges, José Gomes
+ * @brief Header file para o jogo do Bingo
+ * @date 18-02-2025
+ * @file bingo.h
+ */
+
+#ifndef BINGO_H
+#define BINGO_H
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class Bingo{
+
+};
+
+#endif

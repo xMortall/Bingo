33666908bdc92440da45831be555e341257b53e4
jose-gomes1 <145759337+jose-gomes1@users.noreply.github.com>
2025-02-20 19:38:05 -0100

    Update 0.3.5

--- bingo.h
+++ bingo.h
@@ -18,6 +18,7 @@ class Bingo {
         static const int cardsCols = 5;  // Number of columns in the bingo card
         static const int numbersPerColumn = 15;  // Numbers per column
         static bool isCardGenerated;
+        static bool isAutomatic;
 
     public:
         struct Menu {
@@ -26,8 +27,9 @@ class Bingo {
         };
 
         static int numberOfBalls();  // Function to change the number of balls (75, 90, 100)
+        static int gameMode();
 
-        static void playBingo(int ballsQuantity);                // Function to start the bingo game
+        static void playBingo(int ballsQuantity, bool isAutomatic);                // Function to start the bingo game
         static int generateCardsFiles(int ballsQuantity);        // Function to ask the user how many cards to generate
 
         static vector<vector<int>> generateCard(int ballsQuantity);  // Function to generate a single bingo card
